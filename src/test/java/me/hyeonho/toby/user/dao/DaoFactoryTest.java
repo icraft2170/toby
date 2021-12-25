4ad79d8b848760d9f7cb136c3e3de10ab8b1a067
Hero <icraft2170@gmail.com>
2021-12-25 20:47:36 +0900

    1.6 싱글톤레지스트리와 빈 스코프 테스트

---
+++ src/test/java/me/hyeonho/toby/user/dao/DaoFactoryTest.java
@@ -0,0 +1,21 @@
+package me.hyeonho.toby.user.dao;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DaoFactoryTest {
+
+    @Test
+    public void 동등성_테스트(){
+        DaoFactory factory = new DaoFactory();
+        UserDao dao1 = factory.userDao();
+        UserDao dao2 = factory.userDao();
+
+        System.out.println("dao1 = " + dao1);
+        System.out.println("dao2 = " + dao2);
+        Assertions.assertThat(dao1).isNotEqualTo(dao2);
+    }
+
+}
---
+++ src/test/java/me/hyeonho/toby/user/dao/UserDaoTestTest.java
@@ -0,0 +1,25 @@
+package me.hyeonho.toby.user.dao;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTestTest {
+
+    @Test
+    void 스프링컨테이너_싱글톤_테스트(){
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao3 = context.getBean("userDao", UserDao.class);
+        UserDao dao4 = context.getBean("userDao", UserDao.class);
+
+        System.out.println(dao3);
+        System.out.println(dao4);
+
+        assertThat(dao3).isEqualTo(dao4);
+    }
+
+}
