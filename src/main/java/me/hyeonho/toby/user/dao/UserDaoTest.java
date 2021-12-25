5950978491a736a8c6b48e807bfd0573b7722e67
Hero <icraft2170@gmail.com>
2021-12-25 19:38:19 +0900

    1.3.3 관계설정 책임 분리

--- src/main/java/me/hyeonho/toby/user/dao/UserDaoTest.java
+++ src/main/java/me/hyeonho/toby/user/dao/UserDaoTest.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 public class UserDaoTest {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
         ConnectionMaker connectionMaker = new DConnectionMaker();
         UserDao dao = new UserDao(connectionMaker);
 
