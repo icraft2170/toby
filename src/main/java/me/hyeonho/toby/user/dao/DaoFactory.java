33ea1921abcea43140b3015101686f0e3ea2fec1
Hero <icraft2170@gmail.com>
2021-12-25 19:48:02 +0900

    1.4.1 팩토리

---
+++ src/main/java/me/hyeonho/toby/user/dao/DaoFactory.java
@@ -0,0 +1,10 @@
+package me.hyeonho.toby.user.dao;
+
+public class DaoFactory {
+
+    public UserDao userDao(){
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+        UserDao userDao = new UserDao(connectionMaker);
+        return userDao;
+    }
+}
--- src/main/java/me/hyeonho/toby/user/dao/UserDaoTest.java
+++ src/main/java/me/hyeonho/toby/user/dao/UserDaoTest.java
@@ -7,9 +7,7 @@ import java.sql.SQLException;
 public class UserDaoTest {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao dao = new UserDao(connectionMaker);
+        UserDao dao = new DaoFactory().userDao();
 
         User user = new User();
         user.setId("whiteship");
@@ -25,6 +23,5 @@ public class UserDaoTest {
         System.out.println(user2.getPassword());
 
         System.out.println(user2.getId() + " 조회 성공");
-
     }
 }
