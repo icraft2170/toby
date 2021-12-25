92149802db9ce284febe483c2ea20fb3c854b7d5
Hero <icraft2170@gmail.com>
2021-12-25 20:06:33 +0900

    1.4.2 오브젝트 팩토리 활용 - Ver. 2

--- src/main/java/me/hyeonho/toby/user/dao/DaoFactory.java
+++ src/main/java/me/hyeonho/toby/user/dao/DaoFactory.java
@@ -2,8 +2,7 @@ package me.hyeonho.toby.user.dao;
 
 public class DaoFactory {
     public UserDao userDao(){
-        UserDao userDao = new UserDao(connectionMaker());
-        return userDao;
+        return new UserDao(connectionMaker());
     }
 
     private ConnectionMaker connectionMaker() {
