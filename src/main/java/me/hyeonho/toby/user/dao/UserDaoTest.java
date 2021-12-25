7febdcb7fb65ed5d94dd2d338f3d86b9627e7fbd
Hero <icraft2170@gmail.com>
2021-12-25 20:24:33 +0900

    1.5.1 DaoFactory를 사용하는 애플리케이션 컨텍스트

--- src/main/java/me/hyeonho/toby/user/dao/UserDaoTest.java
+++ src/main/java/me/hyeonho/toby/user/dao/UserDaoTest.java
@@ -1,13 +1,18 @@
 package me.hyeonho.toby.user.dao;
 
 import me.hyeonho.toby.user.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context
+                = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao
+                = context.getBean("userDao",UserDao.class);
 
         User user = new User();
         user.setId("whiteship");
