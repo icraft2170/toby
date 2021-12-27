cf434795b00a42308b29defe68198a32dbba52d3
Hero <icraft2170@gmail.com>
2021-12-27 22:12:20 +0900

    2.3.3 포괄적인 테스트

--- src/main/java/me/hyeonho/toby/user/domain/User.java
+++ src/main/java/me/hyeonho/toby/user/domain/User.java
@@ -1,9 +1,12 @@
 package me.hyeonho.toby.user.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class User {
     private String id;
     private String name;
--- src/test/java/me/hyeonho/toby/user/dao/UserDaoTest.java
+++ src/test/java/me/hyeonho/toby/user/dao/UserDaoTest.java
@@ -1,7 +1,6 @@
 package me.hyeonho.toby.user.dao;
 
 import me.hyeonho.toby.user.domain.User;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,9 +8,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-class UserDaoTestTest {
+class UserDaoTest {
 
     @Test
     void 스프링컨테이너_싱글톤_테스트(){
@@ -31,17 +29,48 @@ class UserDaoTestTest {
                 new AnnotationConfigApplicationContext(DaoFactory.class);
 
         UserDao dao = context.getBean("userDao", UserDao.class);
+
+        dao.deleteAll();
+        assertThat(dao.getCount()).isEqualTo(0);
+
+
         User user = new User();
         user.setId("gyumee");
         user.setName("박성철");
         user.setPassword("springno1");
 
         dao.add(user);
+        assertThat(dao.getCount()).isEqualTo(1);
 
         User user2 = dao.get(user.getId());
+
 
         assertThat(user2.getName()).isEqualTo(user.getName());
         assertThat(user2.getPassword()).isEqualTo(user.getPassword());
     }
 
+    @Test
+    void count_테스트() throws SQLException {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(DaoFactory.class);
+
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("gyumee", "박성철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
+        User user3 = new User("bumjin", "박범진", "springno3");
+
+        dao.deleteAll();
+        assertThat(dao.getCount()).isEqualTo(0);
+
+        dao.add(user1);
+        assertThat(dao.getCount()).isEqualTo(1);
+
+        dao.add(user2);
+        assertThat(dao.getCount()).isEqualTo(2);
+
+        dao.add(user3);
+        assertThat(dao.getCount()).isEqualTo(3);
+
+    }
 }
