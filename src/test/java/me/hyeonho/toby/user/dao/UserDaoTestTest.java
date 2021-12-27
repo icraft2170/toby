b5d125ff8dc3058c9494bebe2706ca912ff58a1f
Hero <icraft2170@gmail.com>
2021-12-27 21:52:22 +0900

    2.2 검증 코드 전환 (Junit 5)

--- src/test/java/me/hyeonho/toby/user/dao/UserDaoTestTest.java
+++ src/test/java/me/hyeonho/toby/user/dao/UserDaoTestTest.java
@@ -1,9 +1,12 @@
 package me.hyeonho.toby.user.dao;
 
+import me.hyeonho.toby.user.domain.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +23,25 @@ class UserDaoTestTest {
         System.out.println(dao4);
 
         assertThat(dao3).isEqualTo(dao4);
+    }
+
+    @Test
+    void addAndGet() throws SQLException{
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user = new User();
+        user.setId("gyumee");
+        user.setName("박성철");
+        user.setPassword("springno1");
+
+        dao.add(user);
+
+        User user2 = dao.get(user.getId());
+
+        assertThat(user2.getName()).isEqualTo(user.getName());
+        assertThat(user2.getPassword()).isEqualTo(user.getPassword());
     }
 
 }
