b864fd16a00caad18d5a6f28437e937486c42a48
Hero <81640511+icraft2170@users.noreply.github.com>
2022-07-25 23:33:17 +0900

    6.2.3 프록시 팩토리 빈 사용

---
+++ src/test/java/me/hyeonho/toby/learningtest/jdk/DynamicProxyTest.java
@@ -0,0 +1,60 @@
+package me.hyeonho.toby.learningtest.jdk;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.lang.reflect.Proxy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DynamicProxyTest {
+
+    @Test
+    void simpleProxy() {
+        Hello hello = new HelloTarget();
+        assertThat(hello.sayHello("Toby")).isEqualTo("Hello Toby");
+        assertThat(hello.sayHi("Toby")).isEqualTo("Hi Toby");
+        assertThat(hello.sayThankYou("Toby")).isEqualTo("Thank You Toby");
+
+        Hello proxiedHello = new HelloUppercase(new HelloTarget());
+        assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+
+    @Test
+    void dynamicProxy() {
+        Hello proxiedHello = (Hello) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{Hello.class},
+                new UppercaseHandler(new HelloTarget())
+        );
+        assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+
+    @Test
+    void proxyFactoryBean() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+        pfBean.addAdvice(new UpperCaseAdvice());
+
+        Hello proxiedHello = (Hello) pfBean.getObject();
+        assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
+        assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
+        assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
+    }
+
+    static class UpperCaseAdvice implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            String ref = (String) invocation.proceed();
+            return ref.toUpperCase();
+        }
+    }
+
+}
--- src/test/java/me/hyeonho/toby/learningtest/jdk/ReflectionTest.java
+++ src/test/java/me/hyeonho/toby/learningtest/jdk/ReflectionTest.java
@@ -27,30 +27,4 @@ public class ReflectionTest {
         assertThat((Character) charAtMethod.invoke(name, 0)).isEqualTo('S');
     }
 
-    @Test
-    void simpleProxy() {
-        Hello hello = new HelloTarget();
-        assertThat(hello.sayHello("Toby")).isEqualTo("Hello Toby");
-        assertThat(hello.sayHi("Toby")).isEqualTo("Hi Toby");
-        assertThat(hello.sayThankYou("Toby")).isEqualTo("Thank You Toby");
-
-        Hello proxiedHello = new HelloUppercase(new HelloTarget());
-        assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
-        assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
-        assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
-    }
-
-    @Test
-    void dynamicProxy() {
-        Hello proxiedHello = (Hello) Proxy.newProxyInstance(
-                getClass().getClassLoader(),
-                new Class[]{Hello.class},
-                new UppercaseHandler(new HelloTarget())
-        );
-        assertThat(proxiedHello.sayHello("Toby")).isEqualTo("HELLO TOBY");
-        assertThat(proxiedHello.sayHi("Toby")).isEqualTo("HI TOBY");
-        assertThat(proxiedHello.sayThankYou("Toby")).isEqualTo("THANK YOU TOBY");
-    }
-
-
 }
